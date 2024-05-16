b931a8096fee3040d3d652c0298ba0aaf8bb314d
MthoeSaps <155919550+MthoeSaps@users.noreply.github.com>
2024-05-15 23:21:42 -0700

    Update t1_gases.py

--- t1_gases/t1_gases.py
+++ t1_gases/t1_gases.py
@@ -42,7 +42,7 @@ if selected == "⛽Services Offered":
         st.image(
             img,
             caption="L.P Gas Available ",
-            width=280,
+            width=260,
             channels="RGB")
     with st.container(border=True):
         st.subheader("Gas Handling")
